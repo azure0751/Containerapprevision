e9ce263db3abf72e0750764ae8db8c7bbbccfc66
Azure0751 <58366593+abhi.0751@users.noreply.github.com>
2024-03-16 23:23:20 +0530

    initial check in
    initial check in

---
+++ ConatinerAppRevision/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConatinerAppRevision.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
